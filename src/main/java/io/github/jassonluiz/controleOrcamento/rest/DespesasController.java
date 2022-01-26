fe2196f660c0048ed6a9f007671a8246b00980ea
JassonLuiz <jassonluizjr@hotmail.com>
2022-01-26 01:16:24 -0300

    Adiciona o metodo GET: FindById de Despesas

--- src/main/java/io/github/jassonluiz/controleOrcamento/rest/DespesasController.java
+++ src/main/java/io/github/jassonluiz/controleOrcamento/rest/DespesasController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,7 +38,12 @@ public class DespesasController {
 		return repository.findAll();
 	}
 	
-	
+	@GetMapping(value = "{id}")
+	public ResponseEntity<?> FindById(@PathVariable Integer id){
+		return repository.findById(id)
+							.map(record -> ResponseEntity.ok().body(record))
+							.orElse(ResponseEntity.notFound().build());
+	}
 	
 	
 	
