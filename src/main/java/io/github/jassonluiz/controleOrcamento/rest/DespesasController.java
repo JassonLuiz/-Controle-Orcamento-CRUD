69a9ad5227f2b79f59f5e8797124f3719aa53a35
JassonLuiz <jassonluizjr@hotmail.com>
2022-01-28 23:12:44 -0300

    Adiciona o metodo DELETE: Deleção de despesa

--- src/main/java/io/github/jassonluiz/controleOrcamento/rest/DespesasController.java
+++ src/main/java/io/github/jassonluiz/controleOrcamento/rest/DespesasController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.jassonluiz.controleOrcamento.model.entity.Despesas;
 import io.github.jassonluiz.controleOrcamento.model.repository.DespesasRepository;
@@ -58,6 +60,15 @@ public class DespesasController {
 							}).orElse(ResponseEntity.notFound().build());
 	}
 	
+	@DeleteMapping(value = "{id}")
+	public void delete(@PathVariable Integer id) {
+		repository.findById(id)
+					.map(despesa -> {
+						repository.delete(despesa);
+						return Void.TYPE;
+					})
+					.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Despesa não encontrada!"));
+	}
 	
-	
+		
 }
